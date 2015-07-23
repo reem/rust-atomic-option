c6bca7715164a51931630e4be1c3c29ce88b0e76
Jonathan Reem <jonathan.reem@gmail.com>
2015-07-23 16:30:58 -0700

    (test/doc) Add tests and documentation for all methods.

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,24 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 const NULL: usize = 0;
 
+/// An atomic version of `Option<Box<T>>`, useful for moving owned objects
+/// between threads in a wait-free manner.
 pub struct AtomicOption<T: Send> {
     // Contains the address of a Box<T>, or 0 to indicate None
     inner: AtomicUsize,
-    phantom: PhantomData<Option<T>>
+    phantom: PhantomData<Option<Box<T>>>
 }
 
 impl<T: Send> AtomicOption<T> {
+    /// Create a new AtomicOption storing the specified data.
+    ///
+    /// ```
+    /// # use std::sync::atomic::Ordering;
+    /// # use atomic_option::AtomicOption;
+    /// let opt = AtomicOption::new(Box::new(7));
+    /// let value = opt.take(Ordering::SeqCst).unwrap();
+    /// assert_eq!(value, Box::new(7));
+    /// ```
     #[inline]
     pub fn new(data: Box<T>) -> AtomicOption<T> {
         AtomicOption {
@@ -27,6 +38,15 @@ impl<T: Send> AtomicOption<T> {
         }
     }
 
+    /// Create a new AtomicOption storing None.
+    ///
+    /// ```
+    /// # use std::sync::atomic::Ordering;
+    /// # use atomic_option::AtomicOption;
+    /// let opt: AtomicOption<()> = AtomicOption::empty();
+    /// let value = opt.take(Ordering::SeqCst);
+    /// assert!(value.is_none());
+    /// ```
     #[inline]
     pub fn empty() -> AtomicOption<T> {
         AtomicOption {
@@ -35,16 +55,53 @@ impl<T: Send> AtomicOption<T> {
         }
     }
 
+    /// Take the value out of the AtomicOption, if there is one.
+    ///
+    /// ```
+    /// # use std::sync::atomic::Ordering;
+    /// # use atomic_option::AtomicOption;
+    /// let opt = AtomicOption::new(Box::new(178));
+    /// let first_take = opt.take(Ordering::SeqCst);
+    /// let second_take = opt.take(Ordering::SeqCst);
+    ///
+    /// assert_eq!(first_take, Some(Box::new(178)));
+    /// assert!(second_take.is_none());
+    /// ```
     #[inline]
     pub fn take(&self, ordering: Ordering) -> Option<Box<T>> {
         self.replace(None, ordering)
     }
 
+    /// Swap the value in the AtomicOption with a new one, returning the
+    /// old value if there was one.
+    ///
+    /// ```
+    /// # use std::sync::atomic::Ordering;
+    /// # use atomic_option::AtomicOption;
+    /// let opt = AtomicOption::new(Box::new(1236));
+    /// let old = opt.swap(Box::new(542), Ordering::SeqCst).unwrap();
+    /// assert_eq!(old, Box::new(1236));
+    ///
+    /// let new = opt.take(Ordering::SeqCst).unwrap();
+    /// assert_eq!(new, Box::new(542));
+    /// ```
     #[inline]
     pub fn swap(&self, new: Box<T>, ordering: Ordering) -> Option<Box<T>> {
         self.replace(Some(new), ordering)
     }
 
+    /// Replace the Option in the AtomicOption with a new one, returning the old option.
+    ///
+    /// ```
+    /// # use std::sync::atomic::Ordering;
+    /// # use atomic_option::AtomicOption;
+    /// let opt = AtomicOption::empty();
+    /// let old = opt.replace(Some(Box::new("hello")), Ordering::SeqCst);
+    /// assert!(old.is_none());
+    ///
+    /// let new = opt.take(Ordering::SeqCst).unwrap();
+    /// assert_eq!(new, Box::new("hello"));
+    /// ```
     #[inline]
     pub fn replace(&self, new: Option<Box<T>>, ordering: Ordering) -> Option<Box<T>> {
         let raw_new = new.map(into_raw).unwrap_or(0);
@@ -55,6 +112,26 @@ impl<T: Send> AtomicOption<T> {
         }
     }
 
+    /// Store the new value in the AtomicOption iff it currently contains a None.
+    ///
+    /// None is returned if the store succeeded, or Some is returned with the rejected
+    /// data if the store fails.
+    ///
+    /// This operation is implemented as a single atomic `compare_and_swap`.
+    ///
+    /// ```
+    /// # use std::sync::atomic::Ordering;
+    /// # use atomic_option::AtomicOption;
+    /// let opt = AtomicOption::empty();
+    /// let stored = opt.try_store(Box::new("some data"), Ordering::SeqCst);
+    /// assert!(stored.is_none());
+    ///
+    /// let stored2 = opt.try_store(Box::new("some more data"), Ordering::SeqCst);
+    /// assert_eq!(stored2, Some(Box::new("some more data")));
+    ///
+    /// let value = opt.take(Ordering::SeqCst).unwrap();
+    /// assert_eq!(value, Box::new("some data"));
+    /// ```
     #[inline]
     pub fn try_store(&self, new: Box<T>, ordering: Ordering) -> Option<Box<T>> {
         let raw_new = into_raw(new);
